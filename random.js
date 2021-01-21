5a3f5fb488ef5edefb7c3e10bb165cddf6ef77ab
Mahendra Pratap <ashu4u6@gmail.com>
2021-01-21 08:33:49 +0530

    Update random.js

--- random.js
+++ random.js
@@ -124,3 +124,22 @@ this.then = function (onFulfilled, onRejected) {
       });
     });
   }
+
+
+
+
+function cartesian(...args) {
+    var r = [], max = args.length-1;
+    function helper(arr, i) {
+        for (var j=0, l=args[i].length; j<l; j++) {
+            var a = arr.slice(0); // clone arr
+            a.push(args[i][j]);
+            if (i==max)
+                r.push(a);
+            else
+                helper(a, i+1);
+        }
+    }
+    helper([], 0);
+    return r;
+}
